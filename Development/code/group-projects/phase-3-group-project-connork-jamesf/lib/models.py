47620143d81cf4e2a7622d63e7e9969bcbec9765
Connor Kormos <connorkormos97@gmail.com>
2023-04-18 18:39:18 -0700

    functional tables

--- Development/code/group-projects/phase-3-group-project-connork-jamesf/lib/models.py
+++ Development/code/group-projects/phase-3-group-project-connork-jamesf/lib/models.py
@@ -1,32 +1,34 @@
+#models.py
+
 from sqlalchemy import ForeignKey, Column, DateTime, Integer, String
 
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base
+Base = declarative_base()
 
 class Day(Base):
     __tablename__ = "days"
 
     id = Column(Integer(), primary_key=True)
-    day = Column(String())
+    name = Column(String())
 
-    def __repr__(self):
-        return f"Day(id={self.id}, " + \
-            f"name={self.day})"
+    # def __repr__(self):
+    #     return f"Day(id={self.id}, " + \
+    #         f"name={self.day})"
     
 class Activity(Base):
     __tablename__ = "activities"
     
     id = Column(Integer(), primary_key=True)
     task = Column(String())
-    time_id = Column(Integer(), ForeignKey("time.id"))
-    friend_id = Column(Integer(), ForeignKey("friend.id"))
+    time_id = Column(Integer(), ForeignKey("times.id"))
+    friend_id = Column(Integer(), ForeignKey("friends.id"))
 
-    def __repr__(self):
-        return f"Activity(id={self.id}, " + \
-            f"task={self.task}, " + \
-            f"time_id={self.time_id}, " + \
-            f"friend_id={self.friend_id if self.friend_id else None})"
+    # def __repr__(self):
+    #     return f"Activity(id={self.id}, " + \
+    #         f"task={self.task}, " + \
+    #         f"time_id={self.time_id}, " + \
+    #         f"friend_id={self.friend_id if self.friend_id else None})"
 
 class Time(Base):
     __tablename__ = "times"
@@ -34,9 +36,9 @@ class Time(Base):
     id = Column(Integer(), primary_key=True)
     time = Column(String())
 
-    def __repr__(self):
-        return f"Time(id={self.id}, " + \
-            f"time={self.task})"
+    # def __repr__(self):
+    #     return f"Time(id={self.id}, " + \
+    #         f"time={self.task})"
 
 class Friend(Base):
     __tablename__ = "friends"
@@ -44,6 +46,6 @@ class Friend(Base):
     id = Column(Integer(), primary_key=True)
     name = Column(String())
 
-    def __repr__(self):
-        return f"Friend(id={self.id}, " + \
-            f"name={self.task})"
+    # def __repr__(self):
+    #     return f"Friend(id={self.id}, " + \
+    #         f"name={self.task})"
--- Development/code/group-projects/phase-3-group-project-connork-jamesf/lib/seed.py
+++ Development/code/group-projects/phase-3-group-project-connork-jamesf/lib/seed.py
@@ -1,15 +1,32 @@
-from sqlalchemy import create_engine, desc
+#seed.py
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from models import Day, Activity, Time, Friend
+from lib.models import Base
 
 if __name__ == '__main__':
 
     print("Seeding 🌱...")
     print("Connecting to DB....")
     engine = create_engine('sqlite:///daily_schedule.db')
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    print("Session Created...")
+    print(session, "Session Created...")
 
+    print("Creating day rows...")
+
+    # monday = Day(name="Monday")
+    # tuesday = Day(name="Tuesday")
+    # wednesday = Day(name="Wednesday")
+    # thursday = Day(name="Thursday")
+    # friday = Day(name="Friday")
+    # saturday = Day(name="Saturday")
+    # sunday = Day(name="Sunday")
+
+    # session.add(monday)
+    # # session.add_all([monday, tuesday, wednesday, thursday, friday, saturday, sunday])
+    # session.commit()
+
+    print("All days of the week added!")
     
